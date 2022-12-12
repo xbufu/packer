3c58fd511bb89527e546cc362a1165646dcad645
Colin <colin.schaffer@schaffer-security.com>
2022-12-12 15:45:03 +0100

    Updated config

--- proxmox/linux/scripts/disable-ipv6.sh
+++ proxmox/linux/scripts/disable-ipv6.sh
@@ -4,5 +4,5 @@ export DEBIAN_FRONTEND=noninteractive
 export SILENT=">/dev/null 2>&1"
 # export SILENT="2>&1"
 
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="ipv6.disable=1 quiet"/g' /etc/default/grub
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="/GRUB_CMDLINE_LINUX_DEFAULT="ipv6.disable=1 /g' /etc/default/grub
 eval update-grub $SILENT
--- proxmox/linux/scripts/dotfiles.sh
+++ proxmox/linux/scripts/dotfiles.sh
@@ -6,4 +6,4 @@ echo "Installing chezmoi..."
 $(exec 1>/dev/null 2>/dev/null; sh -c "$(curl -fsLS get.chezmoi.io)" -- -b /usr/local/bin)
 
 echo "Applying dotfiles..."
-chezmoi init --apply --force https://github.com/xbufu/dotfiles.git
+chezmoi init --apply --force https://github.com/xbufu/dotfiles.git 
